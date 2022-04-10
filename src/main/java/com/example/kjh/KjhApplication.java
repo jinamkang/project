3fb66e7b2791f2c30c6b5e29abe4c73d82a586fb
JINAM KANG <jnkang@welcomepayments.co.kr>
2022-04-10 16:01:36 +0900

    두번째 커밋!

--- src/main/java/com/example/kjh/KjhApplication.java
+++ src/main/java/com/example/kjh/KjhApplication.java
@@ -11,3 +11,12 @@ public class KjhApplication {
 	}
 
 }
+
+// git init 깃 프로젝트를 초기화 (없는 경우 생성)
+// -> 로컬 저장소가 생성.(현재 컴퓨터 하드에 git 저장소가 생성됨)
+// git remote add 원격 저장소를 추가
+
+// 원격저장소는 현재 https://github.com/jinamkang/project.git
+
+// commit 커밋, -> 로컬저장소에 변경을 저장
+// push 푸쉬 -> 로컬저장소에 커밋된 내용을 원격저장소로 업로드
---
+++ src/main/java/com/example/kjh/KjhService.java
@@ -0,0 +1,4 @@
+package com.example.kjh;
+
+public class KjhService {
+}
